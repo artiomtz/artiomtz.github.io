9f7a189bde52d94dbb4b775a7711df8e0a84d884
Artiom Tsimkin <artiomtz@gmail.com>
2025-03-05 01:33:47 -0500

    updated Scotiabank employment

--- functions.js
+++ functions.js
@@ -1,156 +1,125 @@
 // JS file - Artiom Tsimkin
 
-// Title type-writer
-var i = 0;
-var txt1 = "Welcome :)";
-var txt2 = "Artiom Tsimkin";
-var speed1 = 150;
-var speed2 = 50;
-var url = document.URL;
+const url = document.URL;
+const titleText1 = 'Welcome :)';
+const titleText2 = 'Artiom Tsimkin';
+const typeSpeed1 = 150;
+const typeSpeed2 = 50;
+let typeCount = 0;
 
-// background animation
-function BackAnimation() {
-  var pathEls = document.querySelectorAll("path");
-  for (var i = 0; i < pathEls.length; i++) {
-    var pathEl = pathEls[i];
-    var offset = anime.setDashoffset(pathEl);
+// Wait for page to load
+window.onload = function () {
+    checkBrowser();
+    setBackgroundAnimation();
+    initMap();
+    setTimeout(typeWelcome, 3000);
+    setTimeout(() => window.scrollTo({ top: 0, behavior: 'smooth' }), 100);
+};
 
-    pathEl.setAttribute("stroke-dashoffset", offset);
-    anime({
-      targets: pathEl,
-      strokeDashoffset: [offset, 0],
-      duration: anime.random(2000, 4000),
-      delay: anime.random(1000, 5000),
-      loop: true,
-      direction: "alternate",
-      easing: "easeInOutSine",
-      autoplay: true,
-    });
-  }
-}
-
-// wait for home to load
-function waitLoad() {
-  //check browser
-  var isChromium = window.chrome;
-  var winNav = window.navigator;
-  var vendorName = winNav.vendor;
-  var isOpera = typeof window.opr !== "undefined";
-  var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-  var isIOSChrome = winNav.userAgent.match("CriOS");
-
-  if (isIOSChrome) {
-    // is Google Chrome on IOS - not alert
-  } else if (
-    isChromium !== null &&
-    typeof isChromium !== "undefined" &&
-    vendorName === "Google Inc." &&
-    isOpera === false &&
-    isIEedge === false
-  ) {
-    // is Google Chrome - not alert
-  } else {
-    // not Google Chrome
-    alert("Keep calm and use Chrome.");
-  }
-
-  // run background animation
-  BackAnimation();
-
-  //type title
-  var delayInMilliseconds = 3000; // delay
-  setTimeout(function () {
-    //code to be executed after delay
-    type1();
-  }, delayInMilliseconds);
-
-  mapboxgl.accessToken =
-    "pk." +
-    "eyJ1IjoiYXJ0aW9tdHoiLCJhIjoi" +
-    "Y2xpNTdtaTUwMGJzcjNlcGg1Z2J3Z" +
-    "jI4cyJ9.Eh195q6LoTu6KsZhgr8psg";
-
-  var map = new mapboxgl.Map({
-    container: "map", // container id
-    style: "mapbox://styles/mapbox/streets-v9",
-    center: [-80.541, 43.468], // starting position
-    zoom: 13, // starting zoom
-  });
-  map.addControl(new mapboxgl.NavigationControl()); // Add zoom and rotation controls to the map.
-
-  // scroll to top on refresh
-  setTimeout(function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, 100);
-}
-
-// type welcome
-function type1() {
-  if (i < txt1.length) {
-    setTimeout(type1, speed1);
-    document.getElementById("top").innerHTML += txt1.charAt(i);
-    i++;
-  } else {
-    i += 3;
-    var delayInMilliseconds = 2000; // delay between titles
-    setTimeout(function () {
-      type2(); //code to be executed after delay
-    }, delayInMilliseconds);
-  }
-}
-
-// delete welcome
-function type2() {
-  if (i > 0) {
-    setTimeout(type2, speed2);
-    document.getElementById("top").innerHTML = document
-      .getElementById("top")
-      .innerHTML.substring(0, i - 1);
-    i--;
-  } else {
-    //i+=3;
-    var delayInMilliseconds = 1000; // delay between titles
-    setTimeout(function () {
-      type3(); //code to be executed after delay
-    }, delayInMilliseconds);
-  }
-}
-
-// type my name
-function type3() {
-  if (i < txt2.length) {
-    setTimeout(type3, speed1);
-    document.getElementById("top").innerHTML += txt2.charAt(i);
-    i++;
-  }
-}
-
-// scroll bar on project page
-function scrollProgress() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 90;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
-// copy button
-function copytext(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-  alert("Copied :)");
-}
-
-// clean url after link clicks in Resume
-window.addEventListener("hashchange", function () {
-  window.history.replaceState(null, null, url);
+// Clean URL after hash change
+window.addEventListener('hashchange', () => {
+    history.replaceState(null, null, url);
 });
 
-// scroll bar on project page
+// Check browser
+function checkBrowser() {
+    const ua = navigator.userAgent;
+    const isChromium = !!window.chrome;
+    const isOpera = typeof window.opr !== 'undefined';
+    const isIEedge = ua.includes('Edge');
+    const isIOSChrome = /CriOS/.test(ua);
+
+    if (!(isIOSChrome || (isChromium && !isOpera && !isIEedge))) {
+        alert('Please use a different browser');
+    }
+}
+
+// Background animation
+function setBackgroundAnimation() {
+    document.querySelectorAll('path').forEach((pathEl) => {
+        const offset = anime.setDashoffset(pathEl);
+        pathEl.setAttribute('stroke-dashoffset', offset);
+        anime({
+            targets: pathEl,
+            strokeDashoffset: [offset, 0],
+            duration: anime.random(2000, 4000),
+            delay: anime.random(1000, 5000),
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+    });
+}
+
+// Scroll progress bar
 window.onscroll = function () {
-  scrollProgress();
+    const winScroll = document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 90;
+    document.getElementById('myBar').style.width = `${scrolled}%`;
 };
+
+// Set copy button
+function copyText(selector) {
+    const element = document.querySelector(selector);
+    if (element) {
+        navigator.clipboard
+            .writeText(element.textContent.trim())
+            .then(() => {
+                alert('Copied :)');
+            })
+            .catch(() => console.error('Error copying text'));
+    } else {
+        console.error('Copy button not found');
+    }
+}
+
+// Initialize Mapbox
+function initMap() {
+    const part1 = 'cGs' + 'uZX' + 'lKMUlq';
+    const part2 = 'b2lZWEowY' + 'Vc5dGRIb' + '2lMQ0po';
+    const part3 = 'SWpvaVkyeHB' + 'OVGR0' + 'yvrv'.toUpperCase();
+    const part4 = ['d01HSnpjak5s', 'Y0dnMVoySjNaakk0', 'Y3lKOS'].join('');
+    const part5 = ['5FaDE5NXE2TG9U', 'dTZLc1', 'poZ3I4cHNn'].join('');
+    const encodedValue = part1 + part2 + part3 + part4 + part5;
+
+    mapboxgl.accessToken = atob(encodedValue);
+    const map = new mapboxgl.Map({
+        container: 'map',
+        zoom: 13,
+        center: [-80.541, 43.468],
+        style: 'mapbox://styles/mapbox/streets-v9',
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+}
+
+// Type Welcome animation
+function typeWelcome() {
+    if (typeCount < titleText1.length) {
+        setTimeout(typeWelcome, typeSpeed1);
+        document.getElementById('top').textContent += titleText1.charAt(typeCount);
+        typeCount++;
+    } else {
+        setTimeout(eraseWelcome, 2000);
+    }
+}
+
+// Erase Welcome animation
+function eraseWelcome() {
+    if (typeCount > 0) {
+        setTimeout(eraseWelcome, typeSpeed2);
+        document.getElementById('top').textContent = titleText1.substring(0, typeCount - 1);
+        typeCount--;
+    } else {
+        setTimeout(typeName, 1000);
+    }
+}
+
+// Type name animation
+function typeName() {
+    if (typeCount < titleText2.length) {
+        setTimeout(typeName, typeSpeed1);
+        document.getElementById('top').textContent += titleText2.charAt(typeCount);
+        typeCount++;
+    }
+}
